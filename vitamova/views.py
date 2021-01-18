db4cf68da29e7d4aa783c02305ad5893d2374549
Ubuntu <ubuntu@ip-172-31-19-133.us-east-2.compute.internal>
2021-01-18 17:38:17 +0000

    reorganized some things, fixed settings file

--- vitamova/views.py
+++ vitamova/views.py
@@ -32,7 +32,7 @@ def check_login(request):
     login_email = request.POST["email"]
     login_token = request.POST["login_token"]
     if login_email != "" and login_token != "":
-        with open("/home/ubuntu/users/userpass.json","r") as f:
+        with open(os.path.join(BASE_DIR,"users/userpass.json"),"r") as f:
             userpass = json.load(f)
     else:
         return 1
@@ -54,13 +54,13 @@ def login(request):
         email = request.POST["email"]
         password = request.POST["password"]
         login_token = request.POST["login_token"]
-        with open("/home/ubuntu/users/userpass.json","r") as f:
+        with open(os.path.join(BASE_DIR,"users/userpass.json"),"r") as f:
             userpass = json.load(f)
         pass_b = bytes(password,encoding="utf-8")
         pash_hash = hashlib.sha256(pass_b).hexdigest()
         if userpass[email]["password"] == pash_hash:
             userpass[email]["token"] = login_token
-            with open("/home/ubuntu/users/userpass.json","w+") as f:
+            with open(os.path.join(BASE_DIR,"users/userpass.json"),"w+") as f:
                 json.dump(userpass,f)
             return HttpResponseRedirect('/dashboard')
         else:
@@ -79,14 +79,14 @@ def signup(request):
         password = request.POST["password"]
         pass_b = bytes(password,encoding="utf-8")
         pass_hash = hashlib.sha256(pass_b).hexdigest()
-        with open("/home/ubuntu/users/userpass.json","r") as f:
+        with open(os.path.join(BASE_DIR,"users/userpass.json"),"r") as f:
             userpass = json.load(f)
         if login_email not in userpass:
             userpass.update({login_email:{"password":pass_hash,"token":""}})
-            with open("/home/ubuntu/users/userpass.json","w+") as f:
+            with open(os.path.join(BASE_DIR,"users/userpass.json"),"w+") as f:
                 json.dump(userpass,f)
             user_data = {"transcribe": {"level": "0"}, "read": {"level": "0"}, "start_date":str(datetime.date.today()),"history":[0]}
-            with open("/home/ubuntu/users/userdata/"+login_email+".json","w+") as f:
+            with open(os.path.join(BASE_DIR,"users/userdata/"+login_email+".json"),"w+") as f:
                 json.dump(user_data,f)
             return HttpResponseRedirect('/login')
         else:
@@ -100,18 +100,18 @@ def dashboard(request):
         login_email = request.POST["email"]
         login_token = request.POST["login_token"]
         if login_email != "" and login_token != "":
-            with open("/home/ubuntu/users/userpass.json","r") as f:
+            with open(os.path.join(BASE_DIR,"users/userpass.json"),"r") as f:
                 userpass = json.load(f)
             user_info = userpass[login_email]
             if user_info["token"] == login_token:
-                with open("/home/ubuntu/users/userdata/"+login_email+".json","r") as f:
+                with open(os.path.join(BASE_DIR,"users/userdata/"+login_email+".json"),"r") as f:
                     user_data = json.load(f)
                 history = user_data["history"]
                 start_date = user_data["start_date"].split("-")
                 date_delta = (datetime.date.today() - datetime.date(int(start_date[0]),int(start_date[1]),int(start_date[2]))).days
                 if len(history) < date_delta+1:
                     history += [0] * (date_delta+1-len(history))
-                    with open("/home/ubuntu/users/userdata/"+login_email+".json","w+") as f:
+                    with open(os.path.join(BASE_DIR, "users/userdata/"+login_email+".json"),"w+") as f:
                         json.dump(user_data,f)
                 if len(history) < 7:
                     week_score = "0"
@@ -141,39 +141,30 @@ def read(request):
     else:
         login_email = request.POST["email"]
         login_token = request.POST["login_token"]
-        with open("/home/ubuntu/users/userpass.json","r") as f:
+        with open(os.path.join(BASE_DIR,"users/userpass.json"),"r") as f:
             userpass = json.load(f)
         user_info = userpass[login_email]
         if user_info["token"] == login_token:
-            with open("/home/ubuntu/users/userdata/"+login_email+".json","r") as f:
+            with open(os.path.join(BASE_DIR,"users/userdata/"+login_email+".json"),"r") as f:
                 user_data = json.load(f)
             if "read" not in user_data:
                 user_data.update({"read":{"level":"0"}})
                 with open("/home/ubuntu/users/userdata/"+login_email+".json","w+") as f:
                     json.dump(user_data,f)
             read_level = int(user_data["read"]["level"])
-            with open("/home/ubuntu/vitamova/content/read_text_"+str(read_level)+".txt","r") as f:
-                read_content = f.read()
+            #This is broken, need to pull content from S3 where I moved the articles
             read_content_l = str(len(re.split(r'[\s-]',read_content)))
             read_content = "<p>" + read_content + "</p>"
             read_content = read_content.replace("\n","</p><p>")
             return(render(request,"read.html",{ "read_text":read_content, "word_count":read_content_l}))
         else:
             return HttpResponseRedirect('/login')
-    
-def transcription_submit(request):
-    return HttpResponseRedirect('/transcribe')
 
 def flashcards(request):
     return render(request,'coming_soon.html',{})
 
 def reader(request):
-    book_title = request.GET['title']
-    if not os.path.isfile('/home/ubuntu/books/'+book_title):
-        s3 = boto3.resource('s3')
-        s3.Object('wkbvitamova', 'books/'+book_title).download_file('/home/ubuntu/books/'+book_title)
-    default_page = "0"
-    return(render(request,'reader.html',{'book_title':book_title,'default_page':default_page}))
+    return render(request,'coming_soon.html',{})
 
 def get_book(request):
     book_title = urllib.parse.unquote(request.GET['title'])
@@ -192,33 +183,35 @@ def translate(request):
 def transcribe(request):
     if request.method == "GET":
         return render(request,'authenticator.html',{"url":"/transcribe/"})
-    else:
-        login_email = request.POST["email"]
-        login_token = request.POST["login_token"]
-        with open("/home/ubuntu/users/userpass.json","r") as f:
-            userpass = json.load(f)
-        user_info = userpass[login_email]
-        if user_info["token"] == login_token:
-            with open("/home/ubuntu/users/userdata/"+login_email+".json","r") as f:
-                user_data = json.load(f)
-            if "transcribe" not in user_data:
-                user_data.update({"transcribe":{"level":"0"}})
-                with open("/home/ubuntu/users/userdata/"+login_email+".json","w+") as f:
-                    json.dump(user_data,f)
-            transcribe_level = int(user_data["transcribe"]["level"])
-            with open("/home/ubuntu/vitamova/content/transcribe_embed.txt","r") as f:
-                transcribe_srcs = f.readlines()
-            transcribe_src = transcribe_srcs[transcribe_level]
-            with open("/home/ubuntu/vitamova/content/transcribe_text_"+str(transcribe_level)+".txt","r") as f:
-                transcription_content = f.read()
-            transcription_content_l = transcription_content.split(" ")
-            for i in range(len(transcription_content_l)):
-                word_bytes = bytes(transcription_content_l[i],encoding="utf-8")
-                transcription_content_l[i] = hashlib.sha256(word_bytes).hexdigest()
-            transcription_content = " ".join(transcription_content_l)
-            return(render(request,"transcribe.html",{"transcribe_src":transcribe_src, "video_content":transcription_content}))
-        else:
-            return HttpResponseRedirect('/login')
+    elif request.method == "POST":
+        if 'request' not in request.POST:
+            logged_in = check_login(request)
+            print(logged_in)
+            if logged_in == 2:
+                return HttpResponseRedirect("/signup")
+            if logged_in == 1:
+                return HttpResponseRedirect("/login")
+            if logged_in == 0:
+                login_email = request.POST["email"]
+                with open(os.path.join(BASE_DIR,"users/userdata/"+login_email+".json"),"r") as f:
+                    user_data = json.load(f)
+                if "transcribe" not in user_data:
+                    user_data.update({"transcribe":{"level":"0"}})
+                    with open("/home/ubuntu/users/userdata/"+login_email+".json","w+") as f:
+                        json.dump(user_data,f)
+                transcribe_level = int(user_data["transcribe"]["level"])
+                with open("/home/ubuntu/vitamova/content/transcribe_embed.txt","r") as f:
+                    transcribe_srcs = f.readlines()
+                transcribe_src = transcribe_srcs[transcribe_level]
+                with open("/home/ubuntu/vitamova/content/transcribe_text_"+str(transcribe_level)+".txt","r") as f:
+                    transcription_content = f.read()
+                transcription_content_l = transcription_content.split(" ")
+                for i in range(len(transcription_content_l)):
+                    word_bytes = bytes(transcription_content_l[i],encoding="utf-8")
+                    transcription_content_l[i] = hashlib.sha256(word_bytes).hexdigest()
+                transcription_content = " ".join(transcription_content_l)
+                return(render(request,"transcribe.html",{"transcribe_src":transcribe_src, "video_content":transcription_content}))
+
 
 def accent(request):
     return render(request,"accent.html",{})
