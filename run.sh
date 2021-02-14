fd08936eee1d18f18271f80445f96757a52bb548
Wesley Belleman <bellemanwesley@gmail.com>
2021-02-13 17:15:58 -0800

    containerized like crazy!

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ sudo podman build --format=docker -t djangod django_v;
 sudo podman build --format=docker -t nginxd nginx;
 sudo podman run -d --network cni-podman1 --ip 10.89.0.2 -p 80:80 nginxd;
 sudo podman run -d --network cni-podman1 --ip 10.89.0.3 djangod;
-sudo podman run -d -e "discovery.type=single-node" --network cni-podman1 --ip 10.89.0.4 -p 9200:9200 localhost/vitamova/elastic;
+sudo podman run -d -e "discovery.type=single-node" --network cni-podman1 --ip 10.89.0.4 localhost/vitamova/elastic;
