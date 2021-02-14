b0df8a950aee88d8eed3806860327f2423ebf168
Wesley Belleman <wesley@wkbsoft.com>
2021-02-14 22:32:17 +0000

    fixed for production

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo dnf install git;
+sudo dnf install podman;
+git clone;
+sudo podman run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.11.0;
+es_id=$(sudo podman container list -aqf ancestor=elasticsearch -f status=running);
+sudo podman commit $es_id vitamova/elastic;
+sudo podman stop $es_id;
+sudo podman network create vitamova --subnet 10.89.0.0/24;
