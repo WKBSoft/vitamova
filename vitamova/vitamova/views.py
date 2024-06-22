a207c57771ab6a04f9aecb65fc96e95f2b6a44ce
Wesley Belleman <bellemanwesley@gmail.com>
2024-06-22 18:10:37 -0400

    adding a page

---
+++ vitamova/vitamova/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse 
+import datetime
+import os
+import sys
+import requests
+from random import randint
+import hashlib
+from random import shuffle
+
+def home(request):
+    return render(request, 'home.html')
