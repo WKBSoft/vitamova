0121ac3adeac41356adab1fd37d44bfa9dac6ee9
Wesley Belleman <bellemanwesley@gmail.com>
2024-06-26 21:24:33 -0400

    updated scraper

---
+++ scrapers/spanish_scraper.py
@@ -0,0 +1,50 @@
+import requests
+import re
+import boto3
+import os
+
+main_page = requests.get("https://laopinion.com/").text
+start = main_page.find("portada__featured")
+link_start = main_page.find("href",start) + 6
+link_end = main_page.find("\"",link_start)
+link = main_page[link_start:link_end]
+article_page = requests.get(link).text
+#The article title starts with the tag <h1 class="story__title">
+title_start = article_page.find("<h1 class=\"story__title\">") + 25
+title_end = article_page.find("</h1>",title_start)
+title = article_page[title_start:title_end]
+#The article text is scattered throughout the page, but it is all contained within the tag <p> which immediately follows a new line character
+text = []
+start = 0
+while start != -1:
+    start = article_page.find("\n<p>",start)
+    if start != -1:
+        end = article_page.find("</p>",start)
+        sub_text = article_page[start+4:end]
+        #Remove any html tags from the text
+        clean = re.compile('<.*?>')
+        sub_text = re.sub(clean, '', sub_text)
+        #Replace &#8221; with "
+        sub_text = sub_text.replace("&#8221;","\"")
+        #If the sub_text starts with Sigue leyendo:, set start to -1 to break the loop
+        if sub_text.startswith("Sigue leyendo:"):
+            start = -1
+        else:
+            text.append(sub_text)
+            start = end
+
+#Make a json object with the title and text
+article = {"title":title,"text":text}
+#Create S3 Session
+#The AWS access key is an environment variable called AWS_ACCESS 
+#The AWS secret key is an environment variable called AWS_SECRET
+aws_access_key_id = os.environ['AWS_ACCESS']
+aws_secret_access_key = os.environ['AWS_SECRET']
+my_session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name="us-east-1"
+)
+#Upload the article to the bucket called evenstarsec.vitamova
+s3 = my_session.resource('s3')
+s3.Bucket('evenstarsec.vitamova').put_object(Key="articles/"+title+".json",Body=str(article))
