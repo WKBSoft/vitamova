e29b5dd870ec78b73f73a2d296779c72ab83c9d6
Wesley Belleman <bellemanwesley@gmail.com>
2024-06-27 21:37:57 -0400

    vocabulary functionality

--- vitamova/vitamova/views.py
+++ vitamova/vitamova/views.py
@@ -87,9 +87,8 @@ def daily_article_step2(request):
     if request.user.is_authenticated:
         #Get the post data called vocabulary
         vocabulary = request.POST.get("vocabulary")
-        #Write the vocabulary to a file
-        with open(os.path.join(BASE_DIR,"vocabulary.txt"),"a") as f:
-            f.write(vocabulary)
+        #Parse the vocabulary from string to json
+        vocabulary = json.loads(vocabulary)
         return render(request,'daily_article/step2.html',{"header":logged_in_header(),"vocabulary":vocabulary})
     else:
         return HttpResponseRedirect("/login/")
