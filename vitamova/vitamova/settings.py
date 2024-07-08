9ca227155c755cbe91c317302f42e1be2bd0a451
Wesley Belleman <bellemanwesley@gmail.com>
2024-07-07 21:30:20 -0400

    finishing touches

--- vitamova/vitamova/settings.py
+++ vitamova/vitamova/settings.py
@@ -39,7 +39,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('django_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost']
 
