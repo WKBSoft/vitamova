14f7ecf0c6cbf7052df95ec4438f8dd529a65650
Wesley Belleman <bellemanwesley@gmail.com>
2024-07-08 16:58:20 -0400

    trying a custom user model

--- vitamova/vitamova/settings.py
+++ vitamova/vitamova/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    #'accounts',
 ]
 
 MIDDLEWARE = [
@@ -132,7 +132,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+#AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
