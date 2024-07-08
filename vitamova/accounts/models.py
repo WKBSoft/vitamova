c9eb43ffb4ef12ffbe827fc9088989eb520b7792
Wesley Belleman <bellemanwesley@gmail.com>
2024-07-08 15:48:05 -0400

    trying a custom user model

--- vitamova/accounts/models.py
+++ vitamova/accounts/models.py
@@ -1,11 +1,5 @@
 # accounts/models.py
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 class CustomUser(AbstractUser):
     pass
-    #points = models.IntegerField(default=0)
-    # add additional fields in here
-
-    def __str__(self):
-        return self.username
