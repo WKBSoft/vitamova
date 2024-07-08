78b6da594196a0cf7686e82e5fd845bf8db10d66
Wesley Belleman <bellemanwesley@gmail.com>
2024-07-08 15:00:02 -0400

    trying a custom user model

--- vitamova/vitamova/settings.py
+++ vitamova/vitamova/settings.py
@@ -93,14 +93,6 @@ WSGI_APPLICATION = 'vitamova.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#Just go with the default sqlite3 database
-ignore = """DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} """
-
 
 DATABASES = {
     "default": {
