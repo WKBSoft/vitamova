494a542278ce367e70eb479cbf9021e42e16a0de
Wesley Belleman <bellemanwesley@gmail.com>
2024-07-09 09:23:42 -0400

    building spanish dictionary

--- vitamova/vitamova/views.py
+++ vitamova/vitamova/views.py
@@ -19,7 +19,7 @@ from openai import OpenAI
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print(vitalib)
+print(BASE_DIR)
 
 #Import vitalib
 sys.path.insert(0, BASE_DIR.parent / 'vitalib')
