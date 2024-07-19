4aeaaafdfe682ed54e5d3eaa07bf1b2eb5dd7069
Wesley Belleman <bellemanwesley@gmail.com>
2024-07-18 21:18:15 -0400

    cleaning up

--- vitamova/vitamova/views.py
+++ vitamova/vitamova/views.py
@@ -74,7 +74,6 @@ def daily_article(request):
         #The article is in the folder articles/spanish
         #The filename is the current date in the format YYYY-MM-DD.json
         obj = s3.Object('evenstarsec.vitamova', 'articles/spanish/'+filename)
-        print(filename)
         article = eval(obj.get()['Body'].read())
         #Make a list of tags for each paragraph named p1, p2, p3, etc.
         paragraphs = []
